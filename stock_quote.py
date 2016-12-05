09e0170ce9b4e1a4a73bc7a230eb0eb8cdadaae4
scripts-richard <scripts.richard@gmail.com>
2016-12-04 22:22:14 -0700

    Broke down into functions and prepped for incorporation of gmail_helper.

--- stock_quote.py
+++ stock_quote.py
@@ -7,98 +7,96 @@
 #-----------------------------------------------------------------------------------
 
 from yahoo_finance import Share
-import smtplib
 import csv
 import time
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import locale
 
-def email(toAddresses, subject, text, html):
-    username = ######### EMAIL TO USE #########
-    password = ######## PASSWORD ########
+import gmail_helper
 
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = username
-    msg['To'] = ', '.join(toAddresses)
+save_file = './stock_quote.csv'
 
-    part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
+def get_existing_info():
+    # ticker -- quantity -- previous price -- previous value
+    stocks = []
+    with open(save_file, newline = '') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            stocks.append(row)
+    return stocks
 
-    msg.attach(part1)
-    msg.attach(part2)
+def get_changes(stocks):
+    changes = []
+    updated_stocks = []
+    for stock in stocks:
+        current_price = Share(stock[0]).get_price()
+        current_value = int(stock[1]) * float(current_price)
+        percent_change = (float(current_price) - float(stock[2])) / float(stock[2])
+        percent_change = round(percent_change, 2)
 
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.ehlo()
-    server.starttls()
-    server.login(username, password)
-    server.sendmail(username, toAddresses, msg.as_string())
-    server.quit()
+        changes.append([stock[0], percent_change, current_value])
+        updated_stocks.append([stock[0], stock[1], current_price, current_value])
+    return changes, updated_stocks
+
+def write_changes(stocks):
+    with open(save_file, 'w', newline = '') as f:
+        writer = csv.writer(f)
+        writer.writerows(stocks)
     return
 
-toAddress = [###### Email List #######]
+def get_emails():
+    with open('emails.txt') as f:
+        emails = f.read().splitlines()
+    for line in emails:
+        if line[0] == '#':
+            emails.remove(line)
+    sender = emails.pop(0)
+    return sender, emails
 
-# ticker -- quantity -- previous price -- previous value
-stocks = []
-save_file = './stock_quote.csv'
-with open(save_file, newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        stocks.append(row)
+def format_text(changes):
+    time = time.strfttime('%Y-%m-%d')
 
-changes = []
+    text = time + '\n\n'
+    change_html = ''
 
-for stock in stocks:
-    currentPrice = Share(stock[0]).get_price()
-    currentValue = int(stock[1]) * float(currentPrice)
-    percentChange = (float(currentPrice) - float(stock[2])) / float(stock[2])
-    percentChange = round(percentChange, 2)
-    stock[2] = currentPrice
-    stock[3] = currentValue
-    changes.append([stock[0], percentChange, currentValue])
+    locale.setlocale(locale.LC_NUMERIC, 'English')
 
-with open(save_file, 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(stocks)
+    for change in changes:
+        dollar_view = locale.format('%.2f', change[2], True)
+        text += change[0] + '     ' + str(change[1]) + '%     $' + dollar_view + '\n'
+        if change[1] > 0:
+            color = 'green'
+        else:
+            color = 'red'
+        change_html += '<tr><td>%s</td>' %change[0]
+        change_html += '<td class="aright %s">%s%%</td>' %(color, change[1])
+        change_html += '<td class="aright">$%s</td></tr>' %dollar_view
 
-time = time.strftime("%Y-%m-%d")
+    html = """\
+    <html>
+        <head>
+            <style>
+                td {padding: 0.5em}
+                .aright {text-align: right}
+                .green {color: green}
+                .red {color: red}
+            </style>
+        </head>
+        <body>
+            <p>%s</p>
+            <br>
+            <table>
+                <tr><th>Ticker</th><th>Change</th><th>Value</th></tr>
+                %s
+            </table>
+        </body>
+    </html>
+    """ %(time, change_html)
+    return text, html
 
-text = "%s\n\n" %time
-changeText = ""
-
-locale.setlocale(locale.LC_NUMERIC, 'English')
-
-for change in changes:
-    dollarView = locale.format('%.2f', change[2], True)
-    text += change[0] + "    " + str(change[1]) + "%    $" + dollarView + "\n"
-    if change[1] > 0.0:
-        color = 'green'
-    else:
-        color = 'red'
-    changeText += '<tr><td>%s</td>' %change[0]
-    changeText += '<td class="aright %s">%s%%</td>' %(color, change[1])
-    changeText += '<td class="aright">$%s</td></tr>' %dollarView
-
-stockHTML = """\
-<html>
-    <head>
-    <style>
-    td {padding: 0.5em}
-    .aright {text-align: right}
-    .green {color: green}
-    .red {color: red}
-    </style>
-    </head>
-    <body>
-        <p>%s</p>
-        <br>
-        <table>
-            <tr><th>Ticker</th><th>Change</th><th>Value</th></tr>
-            %s
-        </table>
-    </body>
-</html>
-""" %(time, changeText)
-
-email(toAddress, "Stocks", text, stockHTML)
+def main():
+    stocks = get_existing_info()
+    changes, stocks = get_changes(stocks)
+    write_changes(stocks)
+    sender, to = get_emails()
+    text, html = format_text(changes)
+    gmail_helper.email(sender, to, 'Stock Quotes', text, html)
