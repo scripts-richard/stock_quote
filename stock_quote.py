2d566f92509cee8f53c4b9e20be78d5ccc45e342
Richard Mills <scripts.richard@gmail.com>
2017-01-05 14:30:11 -0700

    Updated style with flake8.

--- stock_quote.py
+++ stock_quote.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-#-----------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # stock_quote.py
 # v2.0
 # by Richard Mills
 # Fetches select stock prices, compare and save differences, emails changes
-#-----------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 from yahoo_finance import Share
 import csv
@@ -16,15 +16,17 @@ import gmail_helper
 save_file = 'stock_quote.csv'
 emails_file = 'emails.txt'
 
+
 def get_existing_info():
     # Requires an existing file with the following info for eachs stock
     # ticker -- quantity -- previous price -- previous value
     stocks = []
-    with open(save_file, newline = '') as f:
+    with open(save_file, newline='') as f:
         reader = csv.reader(f)
         for row in reader:
             stocks.append(row)
     return stocks
+
 
 def get_changes(stocks):
     # Get current stock info and calculate changes
@@ -33,19 +35,21 @@ def get_changes(stocks):
     for stock in stocks:
         current_price = Share(stock[0]).get_price()
         current_value = int(stock[1]) * float(current_price)
-        percent_change = (float(current_price) - float(stock[2])) / float(stock[2])
+        percent_change = (float(current_price) - float(stock[2])) / float(stock[2]) # noqa
         percent_change = round(percent_change, 2)
 
         changes.append([stock[0], percent_change, current_value])
-        updated_stocks.append([stock[0], stock[1], current_price, current_value])
+        updated_stocks.append([stock[0], stock[1], current_price, current_value]) # noqa
     return changes, updated_stocks
+
 
 def write_changes(stocks):
     # Overwrite existing info with new info
-    with open(save_file, 'w', newline = '') as f:
+    with open(save_file, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(stocks)
     return
+
 
 def format_text(changes):
     # Prepare plain text and html
@@ -58,14 +62,14 @@ def format_text(changes):
 
     for change in changes:
         dollar_view = locale.format('%.2f', change[2], True)
-        text += change[0] + '     ' + str(change[1]) + '%     $' + dollar_view + '\n'
+        text += change[0] + '  ' + str(change[1]) + '%  $' + dollar_view + '\n'
         if change[1] > 0:
             color = 'green'
         else:
             color = 'red'
-        change_html += '<tr><td>%s</td>' %change[0]
-        change_html += '<td class="aright %s">%s%%</td>' %(color, change[1])
-        change_html += '<td class="aright">$%s</td></tr>' %dollar_view
+        change_html += '<tr><td>%s</td>' % change[0]
+        change_html += '<td class="aright %s">%s%%</td>' % (color, change[1])
+        change_html += '<td class="aright">$%s</td></tr>' % dollar_view
 
     html = """\
     <html>
@@ -86,8 +90,9 @@ def format_text(changes):
             </table>
         </body>
     </html>
-    """ %(current_time, change_html)
+    """ % (current_time, change_html)
     return text, html
+
 
 def main():
     stocks = get_existing_info()
@@ -96,6 +101,7 @@ def main():
     text, html = format_text(changes)
     sender, to = gmail_helper.get_emails(emails_file)
     gmail_helper.email(sender, to, 'Stock Quotes', text, html)
+
 
 if __name__ == "__main__":
     main()
